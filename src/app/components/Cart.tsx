baf10cdd3e8b6593336bead453330eb5e6707571
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2025-07-22 21:47:34 -0300

    Next15 E-Commerce v2.3 Sistema de carrinho melhorado

--- src/app/components/Cart.tsx
+++ src/app/components/Cart.tsx
@@ -40,11 +40,10 @@ export default function Cart() {
             </div>
             {useStore.isOpen && (
                 <div onClick={() => useStore.toggleCart()} className="fixed w-full h-screen bg-black/25 left-0 top-0 z-50">
-                    <div onClick={(e) => e.stopPropagation()} className="absolute bg-slate-600 right-0 top-0 w-1/3 h-screen p-12 overflow-y-scroll">
+                    <div onClick={(e) => e.stopPropagation()} className="absolute bg-slate-600 right-0 top-0 w-1/3 h-screen p-12 overflow-y-auto">
                         <h1>Meu carrinho</h1>
                         {useStore.cart.map((item) => (
                             <div key={item.id}>{item.name}</div>
-                            
                         ))}
                     </div>
                 </div>
