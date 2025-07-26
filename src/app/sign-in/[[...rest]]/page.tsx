7f6e4d9b5f395936a02c2e5661667c00355bc790
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2025-07-26 12:35:15 -0300

    Next15 E-Commerce v3.2 Corrigindo bugs de tipos

--- src/app/sign-in/[[...rest]]/page.tsx
+++ src/app/sign-in/[[...rest]]/page.tsx
@@ -1,20 +1,19 @@
 import { SignIn } from "@clerk/nextjs";
 
 type SignInPageProps = {
-    searchParams: {
-        redirectUrl: string;
-    };
-}
+  searchParams: Promise<{
+    redirectUrl?: string;
+  }>;
+};
 
 export default async function SignInPage({ searchParams }: SignInPageProps) {
-  const search = await searchParams;
-  const redirectUrl = search.redirectUrl;
+  const { redirectUrl } = await searchParams;
+
   return (
     <section className="py-14">
       <div className="container mx-auto px-4">
         <div className="flex justify-center">
-          <SignIn signUpUrl="/sign-up" redirectUrl={redirectUrl}/>
-
+          <SignIn signUpUrl="/sign-up" redirectUrl={redirectUrl} />
         </div>
       </div>
     </section>
